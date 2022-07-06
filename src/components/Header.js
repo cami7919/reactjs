0868bbc12ac8ed2afb86faa7fa834d4c8c274f39
Camille Ollivier <camilleollivier@yahoo.fr>
2022-07-06 15:39:23 +0200

    backend_revoir_create_publication

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+const Header = () => {
+    return (
+        <>
+        <div className="header-container">            
+            <img src="/img/logo_groupomania_resized.png" alt="logo groupomania"/>
+            
+        </div>
+        </>
+    );
+};
+
+export default Header;
---
+++ src/components/Nav.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Nav = () => {
+    return (
+        <>
+        <div className="navigation_menu">
+            <ul>
+                <li><a href="#"></a>Inscription</li>
+                <li><a href="#">Connexion</a></li>
+                <li><a href="#">Déconnexion</a></li>
+            </ul>
+            
+            
+        </div>
+        </>
+    );
+};
+
+export default Nav;
